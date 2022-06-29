167f6a8716f63e8c9e0dcbcccf9abde2ec7a70df
Archi5514 <worldofgamesprotanki@gmail.com>
2022-06-30 00:16:57 +0300

    feat: loading from local storage added; modal dialog created

--- app/src/main/java/com/example/randompeopletestapp/presentation/main/view/DetailFragment.kt
+++ app/src/main/java/com/example/randompeopletestapp/presentation/main/view/DetailFragment.kt
@@ -1,10 +1,16 @@
 package com.example.randompeopletestapp.presentation.main.view
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.activityViewModels
 import com.example.randompeopletestapp.R
 import com.example.randompeopletestapp.core.BaseFragment
 import com.example.randompeopletestapp.data.image.loadImage
 import com.example.randompeopletestapp.databinding.FragmentDetailBinding
+import com.example.randompeopletestapp.databinding.ModalDialogPictureBinding
 import com.example.randompeopletestapp.presentation.main.viewmodel.MainViewModel
 import com.example.randompeopletestapp.presentation.main.viewstate.MainViewState
 
@@ -13,6 +19,22 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, MainViewState, MainVi
 ) {
 
     override val viewModel: MainViewModel by activityViewModels()
+
+    private val dialogBinding: ModalDialogPictureBinding by lazy {
+        ModalDialogPictureBinding.inflate(layoutInflater)
+    }
+    private val dialog: Dialog by lazy { Dialog(requireContext()) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.image.setOnClickListener {
+            onImageClicked()
+        }
+    }
 
     override fun renderSuccess(data: MainViewState) {
         data.currentUser?.let {
@@ -35,4 +57,8 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, MainViewState, MainVi
         super.renderSuccess(data)
     }
 
+    private fun onImageClicked() {
+        loadImage(viewModel.currentImage, dialogBinding.largeImage)
+        dialog.show()
+    }
 }
--- app/src/main/java/com/example/randompeopletestapp/presentation/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/randompeopletestapp/presentation/main/viewmodel/MainViewModel.kt
@@ -2,13 +2,18 @@ package com.example.randompeopletestapp.presentation.main.viewmodel
 
 import com.example.randompeopletestapp.core.AppState
 import com.example.randompeopletestapp.core.BaseViewModel
+import com.example.randompeopletestapp.core.di.get
 import com.example.randompeopletestapp.core.di.single
 import com.example.randompeopletestapp.data.worker.UsersUpdateReceiver
 import com.example.randompeopletestapp.domain.entity.appstate.LocalUser
 import com.example.randompeopletestapp.domain.entity.appstate.LocalUsersList
+import com.example.randompeopletestapp.domain.repository.UserRepository
 import com.example.randompeopletestapp.presentation.main.viewstate.MainViewState
+import java.lang.IndexOutOfBoundsException
 
 class MainViewModel : BaseViewModel<MainViewState>(), UsersUpdateReceiver {
+
+    var currentImage: String? = null
 
     private var currentUser: LocalUser? = null
     private var usersList = mutableListOf<LocalUser>()
@@ -17,12 +22,37 @@ class MainViewModel : BaseViewModel<MainViewState>(), UsersUpdateReceiver {
         single { this }
     }
 
+    override fun onViewInit() {
+        super.onViewInit()
+
+        runAsync {
+            val cachedList = get<UserRepository>().getLocalUsersList()
+
+            try {
+                currentUser = cachedList[0]
+            } catch (e: IndexOutOfBoundsException) {
+                return@runAsync
+            }
+
+            usersList.addAll(cachedList)
+            stateLiveData.postValue(
+                AppState.Success(
+                    MainViewState(
+                        LocalUsersList(usersList),
+                        currentUser
+                    )
+                )
+            )
+        }
+    }
+
     override suspend fun updateReceived(users: List<LocalUser>) {
         runAsync {
             currentUser?.let {
                 // initialized - do nothing
             } ?: kotlin.run {
                 currentUser = users[0]
+                currentImage = users[0].largePic
             }
 
             usersList.addAll(users)
@@ -41,6 +71,7 @@ class MainViewModel : BaseViewModel<MainViewState>(), UsersUpdateReceiver {
     fun onUserClicked(user: LocalUser) {
         runAsync {
             currentUser = user
+            currentImage = user.largePic
             stateLiveData.postValue(AppState.Success(MainViewState(currentUser = currentUser)))
         }
     }
